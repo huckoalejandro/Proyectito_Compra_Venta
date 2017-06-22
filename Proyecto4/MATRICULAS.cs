307fe5a51c0507dd02099316e50c6aa6c41aeaf4
FelipeManosalva <manosalva1432@gmail.com>
2017-06-22 08:53:43 -0400

    Creado MATRICULAS.CS
    creacion de clase MATRICULAS con sus respectivos atributos.

---
+++ Proyecto4/MATRICULAS.cs
@@ -0,0 +1,14 @@
+namespace ProyectoEscuelita
+{
+    using System;
+    using System.Collections.Generic;
+    
+    public partial class MATRICULAS
+    {
+        public int MATNUMERO { get; set; }
+        public short TIPCURCODIGO { get; set; }
+        public int ALUMRUT { get; set; }
+        public short SUCCODIGO { get; set; }
+        public Nullable<System.DateTime> MATFECHA { get; set; }
+    }
+}
